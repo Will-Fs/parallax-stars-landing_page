7aea84af758846d63c81ecf968a9a4d0f96d3a17
Will <will.fs.martian@gmail.com>
2022-09-06 01:13:40 -0400

    Stars

--- index.js
+++ index.js
@@ -2,11 +2,38 @@ const root = document.documentElement;
 const buttonBorderColor1Hue = 250;
 const buttonBorderColor2Hue = 360;
 const maxHueShift = 30;
+const maxStarSize = 2;
+const maxStarMove = 30;
+const starBorder = maxStarSize;
+var stars = [];
+
+var lastMousePos = {x: 0, y: 0}
 
 // root.addEventListener("mousemove", e => {
 //   root.style.setProperty('--mouse-x', e.clientX + "px");
 //   root.style.setProperty('--mouse-y', e.clientY + "px");
 // });
+
+const createStars = (numStars) => {
+    const starsContainer = document.querySelector(".stars-container");
+    for (let i = 0; i < numStars; i++) {
+        const newStar = document.createElement("div");
+        const xPos = starBorder + Math.random() * (window.innerWidth - starBorder*2);
+        const yPos = starBorder + Math.random() * (window.innerHeight * 0.8 - starBorder*2);
+        const size = Math.random() * maxStarSize;
+
+        newStar.className = "star";
+        newStar.style = `position: absolute; left: ${xPos}px; top: ${yPos}px; width: ${size}px; height: ${size}px;\
+        box-shadow: 0 0 ${size}px ${size}px white`;
+        starsContainer.appendChild(newStar);
+        stars.push({
+            startPos: {x: xPos, y: yPos},
+            pos: {x: xPos, y: yPos},
+            size: size,
+            div: newStar
+        });
+    }
+}
 
 window.onmousemove = (e) => {
     const buttons = document.querySelectorAll(".navbar-button-border");
@@ -19,9 +46,28 @@ window.onmousemove = (e) => {
     })
     root.style.setProperty("--button-border-color1", `hsl(${hue1}deg, 75%, 60%)`);
     root.style.setProperty("--button-border-color2", `hsl(${hue2}deg, 75%, 60%)`);
+
+    const mouseX = 0.5 - e.clientX / window.innerWidth;
+    const mouseY = 0.5 - e.clientY / window.innerHeight;
+    stars.forEach(star => {
+        star.pos.x = mouseX * maxStarMove * (star.size/maxStarSize) ** 2;
+        star.pos.y = mouseY * maxStarMove * (star.size/maxStarSize) ** 2;
+        star.div.style.left = `${Math.min(Math.max(star.startPos.x - star.pos.x, starBorder), window.innerWidth - starBorder)}px`;
+        star.div.style.top = `${Math.min(Math.max(star.startPos.y - star.pos.y, starBorder), window.innerHeight - starBorder)}px`;
+    })
 }
 
 window.onload = () => {
     root.style.setProperty("--button-border-color1", `hsl(${buttonBorderColor1Hue}deg, 75%, 60%)`);
     root.style.setProperty("--button-border-color2", `hsl(${buttonBorderColor2Hue}deg, 75%, 60%)`);
+
+    createStars(100);
+
+    const navbarChildren = Array.from(document.querySelector(".navbar").children);
+    console.log(navbarChildren);
+    let i = 0.5;
+    navbarChildren.forEach(child => {
+        child.style.animation = `fadeIn 1s ${i}s forwards`
+        i += 0.5;
+    })
 }
